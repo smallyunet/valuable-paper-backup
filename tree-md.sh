426e7e4ee7eaf72f4ae2a3ce9edc828cb912cdc3
smallyunet <499503799@qq.com>
2020-11-23 14:12:17 +0800

    auto

--- tree-md.sh
+++ tree-md.sh
@@ -2,7 +2,7 @@
 
 #File: tree-md
 
-tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
+tree=$(tree -tf --noreport -I '*~|*.md' --charset ascii -v $1 |
        sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1<a href=\"\2\">\4<\/a>:g')
 
 printf "# Project tree\n\n${tree}\n" > README.md
