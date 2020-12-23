5d04817aaf09da7c8d1f7afb63bd4f6f2cedea02
smallyu <wangyudev@qq.com>
2020-12-23 23:20:58 +0800

    auto

--- tree-md.sh
+++ tree-md.sh
@@ -2,7 +2,7 @@
 
 #File: tree-md
 
-tree=$(tree -tf --noreport -I '*~|*.md|*.sh|*.yml' --charset ascii -v $1 |
+tree=$(tree -tf --noreport -I '*~|*.md|*.sh|*.yml|CNAME' --charset ascii -v $1 |
        sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1<a href=\"\2\">\4<\/a>:g')
 
 printf "# Index\n\n${tree}\n" > README.md
